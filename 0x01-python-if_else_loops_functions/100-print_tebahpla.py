6c7f81cb4a4647218330fbeec2150f2c9e1b2505
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-08-23 15:01:35 +0100

    task: 14. Smile in the mirror
    Wrote a program that prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+i = 1
+for char in "zyxwvutsrqponmlkjihgfedcba":
+    value = ord(char)
+    if i % 2 == 0:
+        print("{}".format(chr(value - 32)), end='')
+    else:
+        print("{}".format(char), end='')
+    i = i + 1
