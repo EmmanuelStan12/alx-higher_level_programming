f058abe4c9847066b1f1d1cec7075caf03921aab
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-08-23 16:12:53 +0100

    fix: Task 8. Removed multiple print commands

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,13 +2,14 @@
 def uppercase(str):
     i = 0
     if len(str) == 0:
-        print("{}".format(""))
+        print("{}".format(str))
     for char in str:
         value = ord(char)
         if value >= 97 and value <= 122:
             char = chr(value - 32)
         if i == len(str) - 1:
-            print("{}".format(char))
+            delim = '\n'
         else:
-            print("{}".format(char), end='')
+            delim = ''
+        print("{}".format(char), end=delim)
         i = i + 1
