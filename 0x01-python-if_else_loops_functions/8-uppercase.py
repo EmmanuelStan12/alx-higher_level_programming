ce6ce4603e75c488b3434768fe546a56870c934b
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-08-23 12:50:45 +0100

    task: 8. To uppercase
    Wrote a function that prints a string in uppercase followed by a new line.

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+def uppercase(str):
+    i = 0
+    for char in str:
+        value = ord(char)
+        if value >= 97 and value <= 122:
+            char = chr(value - 32)
+        if i == len(str) - 1:
+            print(char)
+        else:
+            print(char, end = '')
+        i = i + 1 
