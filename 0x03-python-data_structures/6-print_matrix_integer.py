fe4137ed71b61e6e82780d7241acd86b7954b17d
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-08-26 13:34:57 +0100

    task: 6. Lists of lists = Matrix
    Wrote a function that prints a matrix of integers.

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for list in matrix:
+        for i in range(len(list)):
+            space = ' '
+            if i == len(list) - 1:
+                space = '\n'
+            print("{:d}".format(list[i]), end=space)
