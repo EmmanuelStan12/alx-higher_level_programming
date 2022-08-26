1ecbf7606c8bf4567c61c0b9578c0a0ba1436191
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-08-26 13:28:09 +0100

    task: 5. Can you C me now?
    Wrote a function that removes all characters ``c`` and ``C`` from a
    string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    length = len(my_string)
+    new_str = ''
+    for str in my_string:
+        if str != 'c' and str != 'C':
+            new_str = new_str + str
+    return new_str
