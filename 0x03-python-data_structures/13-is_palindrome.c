69c037232f786c79e37e43c2428aa665c87ec920
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-08-28 14:44:31 +0100

    task: Implemented Linked List palindrome
    Wrote a function in C that checks if a singly linked list is a
    palindrome.

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a list is a palindrome
+ * @head: the address of the first element in the list
+ * Return: 1, if it is a palindrome and 0 if otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = NULL, *last_element, *first_element;
+	int size = 0, i = 0;
+
+	if (head == NULL)
+		return (0);
+	current = *head;
+	if (current == NULL)
+		return (1);
+	size = 1;
+	while (current->next != NULL)
+	{
+		current = current->next;
+		size++;
+	}
+	current = *head;
+	while (i < size / 2)
+	{
+		int last_elem = size - i - 1, first_elem = i;
+		int j;
+
+		first_element = current;
+		last_element = current;
+		for (j = 0; j < last_elem + 1; j++)
+		{
+			if (j != first_elem)
+				first_element = first_element->next;
+			if (j != last_elem)
+				last_element = last_element->next;
+		}
+		if (last_element->n != first_element->n)
+			return (0);
+		i++;
+	}
+	return (1);
+}
