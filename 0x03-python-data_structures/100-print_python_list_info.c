b2dac49360275985edd98497362cf4499eb2b8a6
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2022-08-30 09:22:35 -0700

    task: Fixed Errors with task 13

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,6 @@
 #include <Python.h>
-#include <stdio.h>
+#include <object.h>
+#include <listobject.h>
 
 /**
  * print_python_list_info - prints some basic info about python lists.
@@ -10,6 +11,7 @@ void print_python_list_info(PyObject *p)
 {
 	int i, alloc;
 	Py_ssize_t size;
+	PyListObject *obj = (PyListObject *)p;
 
 	size = PyList_Size(p);
 	alloc = ((PyListObject *)p)->allocated;
@@ -20,6 +22,6 @@ void print_python_list_info(PyObject *p)
 		PyObject *item;
 
 		item = PyList_GetItem(p, i);
-		printf("Element %d: %s\n", i, Py_TYPE(item));
+		printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
 	}
 }
