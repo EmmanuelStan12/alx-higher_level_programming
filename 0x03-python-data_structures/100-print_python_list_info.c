a1d93c24f5d8ead93f81e0e9b312ae8fddd6f968
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-08-31 12:00:37 +0100

    task: Added README file
    Added README file for 0x04 Project

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,4 @@
 #include <Python.h>
-#include <object.h>
-#include <listobject.h>
 
 /**
  * print_python_list_info - prints some basic info about python lists.
@@ -9,19 +7,19 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	int i, alloc;
-	Py_ssize_t size;
+	int alloc;
+	Py_ssize_t size, i;
 	PyListObject *obj = (PyListObject *)p;
 
 	size = PyList_Size(p);
 	alloc = ((PyListObject *)p)->allocated;
-	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Size of the Python List = %d\n", (int) size);
 	printf("[*] Allocated = %d\n", alloc);
 	for (i = 0; i < size; i++)
 	{
 		PyObject *item;
 
 		item = PyList_GetItem(p, i);
-		printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
+		printf("Element %d: %s\n", (int) i, Py_TYPE(item)->tp_name);
 	}
 }
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,8 @@
+# 0x04. Python - More Data Structures: Set, Dictionary
+
+Tasks that define how to use sets, dictionaries, lists, lambda functions, map, reduce and filter functions and common iteration mechanisms for different data structures.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
