9208784bd3c6ea968cbe6f9294795d4e9cdb7007
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-12 14:20:16 +0100

    fix: Fixed Error with task 3
    fixed shebang line for import

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,9 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
 def safe_print_division(a, b):
 
     try:
         result = a / b
     except (ValueError, TypeError, ZeroDivisionError):
-        result = None
-    except Exception:
         result = None
     finally:
         print("Inside result: {}".format(result))
