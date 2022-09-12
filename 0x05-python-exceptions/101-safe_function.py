81a208863c9038a71bed1b44862276cecd9750c4
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-12 13:26:20 +0100

    task: Safe function
    Wrote a function that executes a function safely.

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except Exception as exp:
+        print("Exception: {}".format(exp), file=sys.stderr)
+        return None
+    else:
+        return result
