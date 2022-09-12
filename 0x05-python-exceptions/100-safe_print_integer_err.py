be6a695d26432a9fe163ad1fbd02cc9ddc69b383
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-12 14:09:40 +0100

    task: Fixed Checker Errors

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (TypeError, ValueError) as exp:
-        print("Exception: {}".format(exp))
+        print("Exception: {}".format(exp), file=sys.stderr)
         return False
     else:
         return True
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
 def safe_print_division(a, b):
-    result = 0
+
     try:
         result = a / b
     except (ValueError, TypeError, ZeroDivisionError):
         result = None
+    except Exception:
+        result = None
     finally:
         print("Inside result: {}".format(result))
-        return result
+    return result
