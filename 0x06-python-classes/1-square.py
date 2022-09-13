242aee8569488e825ef4968ef87591b9b58dc3a6
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-13 10:04:35 +0100

    task: Square with size
    Wrote a class Square that defines a square with a size attribute.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Note:
+        Square class
+    Args:
+        size (int): the size of the string.
+    """
+    def __init__(self, size):
+        self.__size = size
