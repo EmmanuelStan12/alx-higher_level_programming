9cdee180517bb2c7e2c03ef08cbd0da36ba8262e
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-13 10:26:50 +0100

    task: Access and update private attribute
    Wrote a class Square that defines a square with getters and setter
    attributes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,3 +19,18 @@ class Square:
         finds the area of the square
         """
         return self.__size * self.__size
+
+    @property
+    def size(self):
+        """size: the size property"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets a size based on constraints"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
