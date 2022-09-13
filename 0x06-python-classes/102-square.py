73c87f201836a4ab1158d588c8778c3cbb34e164
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-13 11:17:37 +0100

    task: Compare 2 squares
    Wrote a class Square that overloads comparative operators.

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Note:
+        Square class
+    Args:
+        size (int): the size of the string.
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        finds the area of the square
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """size: the size property"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets a size based on constraints"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def __eq__(self, other):
+        if not isinstance(other, Square):
+            return False
+        return self.__size == other.size
+
+    def __gt__(self, other):
+        if not isinstance(other, Square):
+            return False
+        return self.__size > other.size
+
+    def __lt__(self, other):
+        if not isinstance(other, Square):
+            return False
+        return self.__size < other.size
+
+    def __ge__(self, other):
+        if not isinstance(other, Square):
+            return False
+        return self.__size >= other.size
+
+    def __le__(self, other):
+        if not isinstance(other, Square):
+            return False
+        return self.__size <= other.size
