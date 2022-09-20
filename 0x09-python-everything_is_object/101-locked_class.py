edf48a2f156dc127610d8673009516df3ab59c56
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-20 14:15:06 +0100

    fix: Pycodestyle errors with 101-locked_class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-This module has a class that prevents the user from 
-creating certain attributes
+This module has a class that prevents the user from creating certain attributes
 """
 
 
@@ -10,7 +9,8 @@ class LockedClass():
     Locked class Implementation
     """
     def __setattr__(self, key, value):
+        message = "'LockedClass' object has no attribute '{}'"
         if key != 'first_name':
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(key))
+            raise AttributeError(message.format(key))
         else:
             object.__setattr__(self, key, value)
