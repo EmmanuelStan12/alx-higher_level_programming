2f56c0ab822f4d1243b7d5e94e5b0b2e5e2da576
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-26 11:05:11 +0100

    task: Improve Geometry
    Wrote a class BaseGeometry with area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a class Base Geometry"""
+
+
+class BaseGeometry:
+    """Base Geometry class for geometry"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
