d2cc58fbb760fe6cb339ff2f9c458a069e39451a
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-26 10:55:17 +0100

    task: Same class or inherite from
    Wrote a function that checks if an object is an instance of another.

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains a function that checks for a 
+type class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """This functions checks for a type class
+    """
+    return isinstance(obj, a_class)
