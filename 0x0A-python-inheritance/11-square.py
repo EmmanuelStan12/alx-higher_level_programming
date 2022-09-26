2f85ed39eb22488092b7a2f4bcc99dbc912b9c6f
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-26 11:46:11 +0100

    task: Square #2
    Added method to print the string representation of the square class.

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module contains a Square class that
+inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This class inherits from Rectangle class"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Returns the string representation of the Rectangle"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
