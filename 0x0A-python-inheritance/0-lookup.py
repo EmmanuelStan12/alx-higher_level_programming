622cb429fd502bc8cd7cf5a6bc0481fa59a306a2
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-26 09:58:28 +0100

    task: Lookup
    Wrote a function that returns the list of attributes and methods of an
    object.

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module contains a function that does lookup on a function"""
+
+
+def lookup(obj):
+    """this function does a lookup on a object for it's attrs"""
+    return dir(obj)
