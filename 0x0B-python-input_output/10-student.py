6aa9ec98dc5b28e048fe59b56d0299df8882570d
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-27 12:37:03 +0100

    task: Student to JSON with filter
    Wrote a class Student that defines a student and contains a method that
    converts the class to JSON.

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module contains a class that defines a student"""
+
+
+class Student:
+    """This class defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a student variable"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary representation of the class"""
+        if type(attrs) != list:
+            return self.__dict__
+        new_dict = {}
+        for key in attrs:
+            if (key in self.__dict__) and (key not in new_dict):
+                new_dict[key] = self.__dict__[key]
+        return new_dict
