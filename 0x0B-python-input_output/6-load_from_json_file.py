3becd32fd0cef4890bb0e1fd1d93ca743b3372ba
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-27 11:25:27 +0100

    task: Create object from a JSON file
    Wrote a function that creates an Object from JSON file.

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains a function that creates object from JSON file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """this function creats an object from a file"""
+    str = ""
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            str += line.rstrip().lstrip()
+    try:
+        result = json.loads(str)
+    except Exception as e:
+        raise ValueError(e)
+    return result
