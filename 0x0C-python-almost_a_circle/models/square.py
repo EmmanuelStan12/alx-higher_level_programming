5cd9dc14a2a73ce14de1e251cf7f6c19f1fd61f2
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-29 13:04:08 +0100

    task: Square Update
    Updated the square class by added public getter and setter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,17 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """property for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter for property size"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Returns the string representation of the instance"""
         f = "[Square] ({}) {}/{} - {}"
