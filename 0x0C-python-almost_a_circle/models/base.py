3273c679625c03f693adc94d632061ef6ad401db
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-30 12:03:16 +0100

    task: JSON string to file
    Updated the class Base to be able to save json to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,8 +14,23 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """converts a list of dictionaries into json"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """saves json to a file"""
+        if list_objs is None or len(list_objs) == 0:
+            return
+        result = []
+        for inst in list_objs:
+            inst_name = "{}.json".format(cls.__name__)
+            dict = inst.to_dictionary()
+            result.append(dict)
+        json_str = Base.to_json_string(result)
+        with open(inst_name, 'w', encoding='utf-8') as f:
+            f.write(json_str)
