5bbfcde6683f28af50e7c47412be1454153fdec9
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-11-02 17:41:37 +0100

    task: Always a name

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,4 @@
 -- Creates and Read from a database
 CREATE IF NOT EXISTS DATABASE hbtn_0d_2;
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
