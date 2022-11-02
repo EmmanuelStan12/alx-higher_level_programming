ae32651f2ff6f62b3804f1b9d4ae20219edbebe3
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-11-02 17:51:18 +0100

    task: Cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql.sql
@@ -0,0 +1,4 @@
+-- Creates a database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_GENERATED NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
