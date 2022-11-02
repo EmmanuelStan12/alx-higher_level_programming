551f4ec455d73027c4a55c9ddd7890a01ac4d759
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-11-02 17:48:02 +0100

    task: States table

---
+++ 0x0E-SQL_more_queries/6-states.sql.sql
@@ -0,0 +1,4 @@
+-- Creates a table and database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE AUTO_GENERATED NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
