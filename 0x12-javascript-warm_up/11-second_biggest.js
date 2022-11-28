35a777d753e1667f9a07a1f5ac8565a3b7d07e21
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-11-28 20:13:42 +0100

    task: Second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+if (!process.argv[2]) {
+	console.log(0)
+} else {
+	const length = process.argv.length;
+	const nums = [0, 0];
+	for (let i = 2; i < length; i++) {
+		let num = parseInt(process.argv[i]);
+		if (num > nums[0]) {
+			nums[1] = nums[0];
+			nums[0] = num;
+		} else if (num > nums[1]) {
+			nums[0] = num;
+		}
+	}
+	console.log(nums[1]);
+}
