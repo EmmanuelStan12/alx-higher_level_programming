86fe6e8567faa701070de603149dd1474c8e365e
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-11-29 16:55:09 +0100

    task: Square #2

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const SuperSquare = require('./5-square');
+
+class Square extends SuperSquare {
+	charPrint(c) {
+		const chr = c ? c : 'X';
+		for (let i = 0; i < this.height; i++) {
+			let row = '';
+			for (let j = 0; j < this.width; j++) {
+				row += chr;
+			}
+			console.log(row);
+		}
+	}
+}
+
+module.exports = Square;
