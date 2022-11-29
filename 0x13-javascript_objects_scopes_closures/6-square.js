da021c97ff2f30d87ccd0e76337e8cd73436fedd
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-11-29 16:59:21 +0100

    task: Occurrences

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,16 +3,16 @@
 const SuperSquare = require('./5-square');
 
 class Square extends SuperSquare {
-	charPrint(c) {
-		const chr = c ? c : 'X';
-		for (let i = 0; i < this.height; i++) {
-			let row = '';
-			for (let j = 0; j < this.width; j++) {
-				row += chr;
-			}
-			console.log(row);
-		}
-	}
+  charPrint (c) {
+    const chr = c || 'X';
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += chr;
+      }
+      console.log(row);
+    }
+  }
 }
 
 module.exports = Square;
