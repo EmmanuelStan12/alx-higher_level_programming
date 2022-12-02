98f24f6a2e4c09270cc09c9b7bad3f9fd9924039
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-12-02 22:58:52 +0100

    task: All cities by state

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This file displays all states in mysql
+"""
+import sys
+import MySQLdb
+
+user = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", user=user,
+        port=3306, database=database, password=password)
+    cur = db.cursor()
+    cur.execute("SELECT c.id, c.name, s.name FROM cities AS c INNER JOIN states AS s ON c.state_id = s.id ORDER BY c.id ASC")
+    result = cur.fetchall()
+    for entry in result:
+        print(entry)
+    cur.close()
+    db.close()
