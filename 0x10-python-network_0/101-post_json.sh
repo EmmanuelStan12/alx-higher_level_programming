483959fb0f2e6ff224a568013a7837e45a55069e
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-12-16 00:47:14 +0100

    fix: Post JSON

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This sends a post request to a URL
-curl -X "POST" -H "Content-Type: application/json" -d @"$2" "$1"
+curl -sX "POST" -H "Content-Type: application/json" -d "@$2" "$1"
