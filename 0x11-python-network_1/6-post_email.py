b70a73b2368cd6c1a3e3f204e30c20ba008c6d08
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-12-16 22:21:56 +0100

    task: POST an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This file takes in url as input and gets a header value
+"""
+import requests
+import sys
+
+
+def post_email(url, email):
+    """
+    Posts data to a url
+    """
+    values = {'email': email}
+    return requests.post(url, data=values)
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    result = post_email(url, email).decode('utf-8')
+    print(result.text)
