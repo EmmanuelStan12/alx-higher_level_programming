ec18969787a7cbb495aa76a5621dcd7efe0406a9
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-12-16 22:44:17 +0100

    task: Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This sends a post request to a url
+"""
+import requests
+import sys
+
+
+def search(query, url):
+    """
+    The search function sends a post request
+    """
+    response = requests.post(url, data={'q': query})
+    try:
+        r_json = response.json()
+        id, name = r_json.get('id'), r_json.get('name')
+        if len(r_json) == 0 or not id or not name:
+            return "No result"
+        else:
+            return "[{}] {}".format(id, name)
+    except:
+        return "Not a valid JSON"
+
+
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+    query = sys.argv[1] if len(sys.argv) == 2 else ""
+    data = search(query, url)
+    print(data)
