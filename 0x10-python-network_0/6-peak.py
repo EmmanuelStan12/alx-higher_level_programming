f9aa8a4462c5d503c68765562d31b1c077e77d53
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-12-16 01:32:09 +0100

    task: Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 This file contains a function that finds the peak
 """
 
+
 def find_peak(list_of_integers):
     """
     This function finds the peak value in the array
@@ -12,18 +13,12 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 1:
         return list_of_integers[0]
     nums = list_of_integers
-    f = int(len(nums) / 2) - 1
-    l = int(len(nums) / 2)
-    while l < len(nums):
-        if f == 0 and nums[f] > nums[f + 1]:
-            return nums[f]
-        elif l == len(nums) - 1 and nums[l] >= nums[l - 1]:
-            return nums[l]
-        elif f > 0:
-            if nums[f] > nums[f + 1] and nums[f] >= nums[f - 1]:
-                return nums[f]
-        if nums[l] > nums[l - 1] and nums[l] >= nums[l + 1]:
-            return nums[l]
-        l = l + 1
-        f = f - 1
+    for i in range(0, len(nums)):
+        if i > 0 and i < len(nums) - 1:
+            if nums[i - 1] <= nums[i] and nums[i + 1] <= nums[i]:
+                return nums[i]
+        if i == 0 and nums[i + 1] <= nums[i]:
+            return nums[i]
+        if i == len(nums) - 1 and nums[i - 1] <= nums[i]:
+            return nums[i]
     return None
