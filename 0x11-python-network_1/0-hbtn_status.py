216fdb0331bc862e550368a91d7b6fb62e1af60f
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-12-16 21:31:17 +0100

    task: What's my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This file contains a function that does network operations
+"""
+import urllib.request
+
+
+def fetch(url):
+    """
+    This fetches information from the url
+    """
+    with urllib.request.urlopen(url) as response:
+        result = response.read()
+    return result
+
+if __name__ == '__main__':
+    response = fetch('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
+    print("\t- utf8 content: {}".format(response.decode('utf-8')))
